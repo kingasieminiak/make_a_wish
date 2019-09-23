fc05eba36b6bd056b731a5aa434b7ebd14e8dffc
Kinga Sieminiak <kinga.sieminiak@intive.com>
2019-09-24 00:33:08 +0200

    Allow user to trigger action on touch

--- src/scripts/makeAWish.js
+++ src/scripts/makeAWish.js
@@ -2,12 +2,17 @@ export default function makeAWish() {
   const mouth = document.querySelector('.jsMounth');
   const flame = document.querySelector('.jsFlame');
 
-  document.addEventListener('keypress', function(e) { // eslint-disable-line
-    if (e.keyCode === 13) {
-      mouth.classList.add('resize');
+  function handleUserAction() {
+    mouth.classList.add('resize');
+    setTimeout(() => flame.classList.add('hidden'), 100); // eslint-disable-line
+    setTimeout(() => mouth.classList.remove('resize'), 900); // eslint-disable-line
+  }
 
-      setTimeout(() => flame.classList.add('hidden'), 100); // eslint-disable-line
-      setTimeout(() => mouth.classList.remove('resize'), 900); // eslint-disable-line
-    }
+  document.addEventListener('keypress', function(e) { // eslint-disable-line
+    if (e.keyCode === 13) handleUserAction();
+  });
+
+  document.addEventListener('touchend', function(e) { // eslint-disable-line
+    handleUserAction();
   });
 }
