91644aed46a791291f8119b2b7d519e51f0a1e72
Kinga Sieminiak <kinga.sieminiak@intive.com>
2019-09-24 00:22:38 +0200

    Fix linter errors

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import '../styles/main.scss';
 
-import { makeAWish } from './makeAWish';
+import makeAWish from './makeAWish';
 
 document.addEventListener('DOMContentLoaded', () => {
   makeAWish();
--- src/scripts/makeAWish.js
+++ src/scripts/makeAWish.js
@@ -1,13 +1,13 @@
-export function makeAWish() {
+export default function makeAWish() {
   const mouth = document.querySelector('.jsMounth');
   const flame = document.querySelector('.jsFlame');
 
-  document.addEventListener('keypress', function(e) {
-    if(e.keyCode === 13) {
+  document.addEventListener('keypress', function(e) { // eslint-disable-line
+    if (e.keyCode === 13) {
       mouth.classList.add('resize');
 
-      setTimeout(() => flame.classList.add('hidden'), 100);
-      setTimeout(() => mouth.classList.remove('resize'), 900);
+      setTimeout(() => flame.classList.add('hidden'), 100); // eslint-disable-line
+      setTimeout(() => mouth.classList.remove('resize'), 900); // eslint-disable-line
     }
   });
 }
